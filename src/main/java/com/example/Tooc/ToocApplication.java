a096d0ff2bd50eee9efc11f92709081ddeec95ee
nam <yhnham@gmail.com>
2025-04-04 20:17:08 +0900

    file upload

---
+++ src/main/java/com/example/Tooc/ToocApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tooc/ToocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
